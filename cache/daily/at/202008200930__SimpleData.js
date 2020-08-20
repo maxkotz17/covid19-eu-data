861fd68acfbac2f92db8c37adc3305c0956a3b01
GitHub Action <action@github.com>
2020-08-20 11:17:57 +0000

    Update AT Dataset

--- cache/daily/at/202008200930__SimpleData.js
+++ cache/daily/at/202008200930__SimpleData.js
@@ -1,3 +1,3 @@
-var Erkrankungen = "24403";
-var LetzteAktualisierung = "20.08.2020 12:00:00";
-var SimpleDataVersion = "20.08.2020 12:00:00 V 1.2.3.0"
+var Erkrankungen = "24421";
+var LetzteAktualisierung = "20.08.2020 13:00:00";
+var SimpleDataVersion = "20.08.2020 13:00:00 V 1.2.3.0"
