a035cbecbc32dd8061cd101ed7ef547d84525888
GitHub Action <action@github.com>
2020-09-07 13:21:02 +0000

    Update AT Dataset

---
+++ cache/daily/at/202009071500__Geschlechtsverteilung.js
@@ -0,0 +1,2 @@
+var dpGeschlechtsverteilung = [{"label":"weiblich","y":49,"CSV":"weiblich;49"},{"label":"männlich","y":51,"CSV":"männlich;51"}];
+var GeschlechtsverteilungVersion = "07.09.2020 15:00:00 V 1.2.3.0"
---
+++ cache/daily/at/202009071500__SimpleData.js
@@ -0,0 +1,3 @@
+var Erkrankungen = "29559";
+var LetzteAktualisierung = "07.09.2020 15:00:00";
+var SimpleDataVersion = "07.09.2020 15:00:00 V 1.2.3.0"
